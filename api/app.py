d8c2a45ec193c8c1e944706ed054bb9aac791562
florsalvador <florsalvador219@gmail.com>
2025-07-17 10:49:38 -0700

    feat: add GET /products and GET/ users endpoints

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -27,6 +28,32 @@ class Orders(db.Model):
         self.data_entry = data_entry
         self.date_processed = date_processed
 
+class Products(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    price = db.Column(db.Integer)
+    image = db.Column(db.String())
+    type = db.Column(db.String())
+    date_entry = db.Column(db.DateTime, nullable=True)
+    def __init__(self, name, price, image, type, date_entry):
+        self.name = name
+        self.price = price
+        self.image = image
+        self.type = type
+        self.date_entry = date_entry
+
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
+    def __init__(self, role, email, password):
+        self.role = role
+        self.email = email
+        self.password = password
+
 @app.route("/")
 def index():
     return "Index"
@@ -46,6 +73,60 @@ def get_orders():
         }
         results.append(info)
     return {"orders": results}
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    products = Products.query.all()
+    results = []
+    for product in products:
+        info = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "image": product.image,
+            "type": product.type,
+            "date_entry": product.date_entry
+        }
+        results.append(info)
+    return {"products": results}
+
+@app.route("/products", methods=["POST"])
+def create_products():
+    if request.is_json:
+        data = request.get_json()
+        date_entry_str = data.get("date_entry")
+        if date_entry_str:
+            try:
+                date_entry = datetime.fromisoformat(date_entry_str)
+            except ValueError:
+                return {"error": "Invalid date format. Use ISO format like '2024-07-16T12:00:00'"}, 400
+        else:
+            date_entry = None
+        new_product = Products(
+            name=data["name"],
+            price=data["price"],
+            image=data["image"],
+            type=data["type"],
+            date_entry=date_entry
+        )
+        db.session.add(new_product)
+        db.session.commit()
+        return {"message": f"Product {new_product.name} has been created successfully."}
+    else:
+        return {"error": "The request payload is not in JSON format"}, 400
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = Users.query.all()
+    results = [
+        {
+            "id": user.id,
+            "role": user.role,
+            "email": user.email,
+            "password": user.password
+        } for user in users
+    ]
+    return {"users": results}
 
 if __name__ == "__main__":
     app.run(debug=True)
