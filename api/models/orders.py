aba01353637e32293b880f3a3f1d26bb3cbbd87c
florsalvador <florsalvador219@gmail.com>
2025-07-18 11:00:43 -0700

    feat: add relationship in Orders model to OrderProducts

--- api/models/orders.py
+++ api/models/orders.py
@@ -2,17 +2,11 @@ from api.db import db
 
 class Orders(db.Model):
     __tablename__ = "orders"
-    
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     client = db.Column(db.String())
     status = db.Column(db.String())
-    data_entry = db.Column(db.DateTime)
+    date_entry = db.Column(db.DateTime)
     date_processed = db.Column(db.DateTime)
-
-    def __init__(self, user_id, client, status, data_entry, date_processed):
-        self.user_id = user_id
-        self.client = client
-        self.status = status
-        self.data_entry = data_entry
-        self.date_processed = date_processed
+    products_list = db.relationship("OrderProducts", back_populates="order", cascade="all, delete-orphan")
